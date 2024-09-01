0ce46f8c1ed28753d995de28fa9d28a9d9e265b2
sgleoni <sl9067@outlook.it>
2024-09-01 08:40:58 +0200

    2.18 How to use GetComponent

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -7,8 +7,18 @@ public class Delivery : MonoBehaviour
 {
     bool hasPackage = false;
 
-    [SerializeField]
-    float timer = 1f;
+    [SerializeField] Color32 hasPackageColor = new Color32(255, 255, 255, 255);
+
+    [SerializeField] Color32 noPackageColor = new Color32(255, 255, 255, 255);
+
+    [SerializeField] float timer = 1f;
+
+    SpriteRenderer spriteRenderer;
+
+    private void Start() 
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();    
+    }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
@@ -22,11 +32,13 @@ public class Delivery : MonoBehaviour
             Debug.Log("Package picked up");
             hasPackage = true;
             Destroy(other.gameObject, timer);
+            spriteRenderer.color = hasPackageColor;
         } 
         else if(other.CompareTag("Customer") && hasPackage)
         {
             Debug.Log("Delivered package");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
     }
 }
